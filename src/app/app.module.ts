c8792c9b5b2c1c3c15169fc4c4410205fc80eba1
Rheza Haryo Hanggara <rhezahanggara@gmail.com>
2019-01-30 12:23:03 +0700

    ''

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { ArticleService } from './article/article.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ArticleRoutingModule } from './article-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ArticleRoutingModule
   ],
-  providers: [],
+  providers: [ArticleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
